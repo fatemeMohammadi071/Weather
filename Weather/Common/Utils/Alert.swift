9f4f2f9be6c2a6a549168e5060fc6e14bcf33ab4
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2022-03-30 20:11:00 +0430

    Added utils

---
+++ Weather/Common/Utils/Alert.swift
@@ -0,0 +1,18 @@
+//
+//  Alert.swift
+//  Weather
+//
+//  Created by Fateme on 1/7/1401 AP.
+//
+
+import UIKit
+
+public protocol Alertable {}
+public extension Alertable where Self: UIViewController {
+    
+    func showAlert(title: String = "", message: String, preferredStyle: UIAlertController.Style = .alert, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: completion)
+    }
+}
