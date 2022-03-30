8611614bbf4b00ad943d94e1a0b3e9470cacc0d8
Fateme Mohammadi <mohammadi_f36@yahoo.com>
2022-03-30 20:11:20 +0430

    Added app configuration

---
+++ Weather/Application/AppConfigurations.swift
@@ -0,0 +1,23 @@
+//
+//  AppConfigurations.swift
+//  Weather
+//
+//  Created by Fateme on 1/7/1401 AP.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+    lazy var imagesBaseURL: String = {
+        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return imageBaseURL
+    }()
+}
